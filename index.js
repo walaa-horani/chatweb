3fe3ec8b5a52279499bd0c325b71314367415b51
unknown <walaa.horani@hotmail.com>
2024-08-11 13:50:22 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const { default: axios } = require("axios");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    try {
+      const r = await axios.put("https://api.chatengine.io/users/", 
+        { username: username, secret: username, first_name: username },
+        { headers: { "private-key": "4ba3748d-6ebe-4faf-9283-8440b7a5024f " } }
+      );
+      return res.status(r.status).json(r.data);
+    } catch (e) {
+      console.error("Error occurred:", e);
+      return res.status(e.response?.status || 500).json(e.response?.data || { error: "Internal Server Error" });
+    }
+  });
+  
+
+app.listen(3001);
